9b9aa9806efbf1dc6ad42514e38aded5ab689948
Benoit Quenaudon <bquenaudon@squareup.com>
2020-03-06 16:38:02 -0500

    WIP

--- wire-protoc-compatibility-tests/src/test/java/com/squareup/wire/Proto3WireProtocCompatibilityTests.kt
+++ wire-protoc-compatibility-tests/src/test/java/com/squareup/wire/Proto3WireProtocCompatibilityTests.kt
@@ -17,12 +17,20 @@
 
 package com.squareup.wire
 
+import com.google.protobuf.Any
 import com.google.protobuf.DescriptorProtos
 import com.google.protobuf.FieldOptions
+import com.google.protobuf.util.JsonFormat
+import com.squareup.moshi.Moshi
 import com.squareup.wire.proto3.requiredextension.RequiredExtension
 import com.squareup.wire.proto3.requiredextension.RequiredExtensionMessage
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import squareup.proto3.pizza.BuyOneGetOnePromotion
+import squareup.proto3.pizza.FreeGarlicBreadPromotion
+import squareup.proto3.pizza.Pizza
+import squareup.proto3.pizza.PizzaDelivery
+import squareup.proto3.pizza.PizzaOuterClass
 
 class Proto3WireProtocCompatibilityTests {
   // Note: this test mostly make sure we compile required extension without failing.
@@ -40,4 +48,86 @@ class Proto3WireProtocCompatibilityTests {
     assertThat(DescriptorProtos.FieldOptions.newBuilder().build()).isNotNull()
     assertThat(FieldOptions()).isNotNull()
   }
+
+  @Test fun protocJson() {
+    val pizzaDelivery = PizzaOuterClass.PizzaDelivery.newBuilder()
+        .setAddress("507 Cross Street")
+        .addPizzas(PizzaOuterClass.Pizza.newBuilder()
+            .addToppings("pineapple")
+            .addToppings("onion")
+            .build())
+        .setPromotion(Any.pack(PizzaOuterClass.BuyOneGetOnePromotion.newBuilder()
+            .setCoupon("MAUI")
+            .build()))
+        .build()
+
+    val json = """
+        |{
+        |  "address": "507 Cross Street",
+        |  "pizzas": [{
+        |    "toppings": ["pineapple", "onion"]
+        |  }],
+        |  "promotion": {
+        |    "@type": "type.googleapis.com/squareup.proto3.pizza.BuyOneGetOnePromotion",
+        |    "coupon": "MAUI"
+        |  }
+        |}
+        """.trimMargin()
+
+    val typeRegistry = JsonFormat.TypeRegistry.newBuilder()
+        .add(PizzaOuterClass.BuyOneGetOnePromotion.getDescriptor())
+        .add(PizzaOuterClass.FreeGarlicBreadPromotion.getDescriptor())
+        .build()
+
+    val jsonPrinter = JsonFormat.printer()
+        .usingTypeRegistry(typeRegistry)
+    assertThat(jsonPrinter.print(pizzaDelivery)).isEqualTo(json)
+
+    val jsonParser = JsonFormat.parser().usingTypeRegistry(typeRegistry)
+    val parsed = PizzaOuterClass.PizzaDelivery.newBuilder()
+        .apply { jsonParser.merge(json, this) }
+        .build()
+    assertThat(parsed).isEqualTo(pizzaDelivery)
+  }
+
+  @Test fun wireJson() {
+    val wireMessage = PizzaDelivery(
+        address = "507 Cross Street",
+        pizzas = listOf(Pizza(toppings = listOf("pineapple", "onion"))),
+        promotion = AnyMessage.pack(BuyOneGetOnePromotion(coupon = "MAUI"))
+    )
+    val json = """
+        |{
+        |  "address": "507 Cross Street",
+        |  "pizzas": [
+        |    {
+        |      "toppings": [
+        |        "pineapple",
+        |        "onion"
+        |      ]
+        |    }
+        |  ],
+        |  "promotion": {
+        |    "@type": "type.googleapis.com/squareup.proto3.pizza.BuyOneGetOnePromotion",
+        |    "coupon": "MAUI"
+        |  }
+        |}
+        """.trimMargin()
+
+    val moshi = Moshi.Builder()
+        .add(WireJsonAdapterFactory()
+            .plus(BuyOneGetOnePromotion.ADAPTER, FreeGarlicBreadPromotion.ADAPTER))
+        .build()
+
+    val jsonAdapter = moshi.adapter(PizzaDelivery::class.java).indent("  ")
+    assertThat(jsonAdapter.toJson(wireMessage)).isEqualTo(json)
+    assertThat(jsonAdapter.fromJson(json)).isEqualTo(wireMessage)
+  }
+
+  // Wire -> Google, Google -> Wire
+  //
+
+  // Nice error on unrecognized @type
+  // Nice error on absent @type
+  // Nice error on unregistered type
 }
